b0f263a7536a1efeb5a97a39ddbabf0b08c9db8a
komp <komp@DESKTOP-QJ8EB25>
2023-05-18 08:52:14 +0200

    Add project files.

---
+++ guessing game/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace guessing_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string SecretWord = "okoń";
+            string guess = "";
+            int guessCount = 0;
+            int guesslimit = 3;
+            bool outOfGuesses = false;
+            while (guess != SecretWord && !outOfGuesses)
+            {
+                if (guessCount < guesslimit)
+                {
+                    Console.WriteLine("Jakie jest hasło?");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+
+                }
+            }
+            if ( outOfGuesses ) 
+            {
+                Console.WriteLine("Koniec prób. Przegrałeś");
+            }else 
+            {
+                Console.WriteLine("Brawo, ja bym Panu dał nagrodę");
+            }
+            Console.ReadLine();
+        }
+    }
+}
